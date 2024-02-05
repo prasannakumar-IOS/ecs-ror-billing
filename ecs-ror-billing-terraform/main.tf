0a5d8e6e83ea69abf032154fc71cb6ab073d2585
prasannakumar-IOS <95841494+prasannakumar-IOS@users.noreply.github.com>
2024-02-05 10:48:17 +0530

    Update main.tf

--- ecs-ror-billing-terraform/main.tf
+++ ecs-ror-billing-terraform/main.tf
@@ -111,12 +111,8 @@ resource "aws_security_group" "ecs_node_sg" {
 
 # --- ECS Launch Template ---
 
-data "aws_ssm_parameter" "ecs_node_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
-}
-
 resource "aws_launch_template" "ecs_ec2" {
-  name_prefix            = "biling-ecs-ec2-"
+  name_prefix            = "billing-ecs-ec2-"
   image_id               = data.aws_ssm_parameter.ecs_node_ami.value
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ecs_node_sg.id]
