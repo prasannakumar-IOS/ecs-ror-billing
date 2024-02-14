41f6a11f43c0bee8bc31675bf97e1e839e4c9cde
prasannakumar-IOS <95841494+prasannakumar-IOS@users.noreply.github.com>
2024-02-14 16:14:08 +0530

    Update variable.tf

--- ecs-ror-billing-terraform/variable.tf
+++ ecs-ror-billing-terraform/variable.tf
@@ -15,82 +15,82 @@ variable "vpc_cidr_tag" {
 
 variable "public_subnet_tag" {
   type        = string
-  description = "Public subnet tag string"
+  description = "Public subnet tag string."
 }
 
 variable "internet_gateway_tag" {
   type        = string
-  description = "Internet gateway tag string"
+  description = "Internet gateway tag string."
 }
 
 variable "epi_tag" {
   type        = string
-  description = "EPI tag string"
+  description = "EPI tag string."
 }
 
 variable "route_table_tag" {
   type        = string
-  description = "Route table tag string"
+  description = "Route table tag string."
 }
 
 variable "ecs_cluster_name" {
   type        = string
-  description = "ECS cluster name string"
+  description = "ECS cluster name string."
 }
 
 variable "security_groups_name" {
   type        = string
-  description = "ECS cluster name string"
+  description = "ECS cluster name string."
 }
 
 variable "launch_template_name" {
   type        = string
-  description = "Launch template name string"
+  description = "Launch template name string."
 }
 
 variable "launch_template_instance_type" {
   type        = string
-  description = "Launch template instance type"
+  description = "Launch template instance type."
 }
 
 variable "auto_scaling_group_name" {
   type        = string
-  description = "Auto scaling group name string"
+  description = "Auto scaling group name string."
 }
 
 variable "auto_scaling_group_min_size" {
   type        = number
-  description = "Auto scaling group minimum size"
+  description = "Auto scaling group minimum size."
 }
 
 variable "auto_scaling_group_max_size" {
   type        = number
-  description = "Auto scaling group maximum size"
+  description = "Auto scaling group maximum size."
 }
 
 variable "ecs_capacity_provider_name" {
   type        = string
-  description = "ECS capacity provider name string"
+  description = "ECS capacity provider name string."
 }
 
 variable "ecs_task_definition_family_name" {
   type        = string
-  description = "ecs task definition family name string"
+  description = "ecs task definition family name string."
 }
 
 variable "ecs_task_definition_family_cpu" {
   type        = number
-  description = "ecs task definition family cpu"
+  description = "ecs task definition family cpu."
 }
 
 variable "ecs_task_definition_family_memory" {
   type        = number
-  description = "ecs task definition family memory"
+  description = "ecs task definition family memory."
 }
 
 variable "ecs_service_name" {
   type        = string
-  description = "ecs service name string"
+  description = "ecs service name string."
 }
 
 variable "ecs_alb_name" {
