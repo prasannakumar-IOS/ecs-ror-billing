882a5c6382991b029ced5725cf372e84cffb53f5
prasannakumar-IOS <95841494+prasannakumar-IOS@users.noreply.github.com>
2024-02-15 13:41:44 +0530

    Update vpc.tf

--- ecs-ror-billing-terraform/Networking/vpc.tf
+++ ecs-ror-billing-terraform/Networking/vpc.tf
@@ -53,3 +53,9 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+resource "aws_ssm_parameter" "vpc_id" {
+  name  = "billing1-vpc-id"
+  type  = "String"
+  value = aws_vpc.vpc.id
+}
