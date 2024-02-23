50cc586dc5a7a9cbf4d8d2033ee84daecdb58ed4
prasannakumar-IOS <95841494+prasannakumar-IOS@users.noreply.github.com>
2024-02-23 10:31:54 +0530

    Update asg.tf

--- ecs-ror-billing-terraform/Server/asg.tf
+++ ecs-ror-billing-terraform/Server/asg.tf
@@ -1,9 +1,5 @@
 # --- ECS Launch Template ---
 
-data "aws_ssm_parameter" "ecs_node_sg_id" {
-  name = "billing1-ecs-node-sg-id"
-}
-
 data "aws_ssm_parameter" "ecs_node_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
